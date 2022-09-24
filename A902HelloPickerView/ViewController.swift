05645f674f98f1ce8b9df4632b059c323eab38d7
Danny Shen <shenfive@gmail.com>
2022-09-24 15:59:27 +0800

    DataSource and Delegate

--- A902HelloPickerView/ViewController.swift
+++ A902HelloPickerView/ViewController.swift
@@ -7,22 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDataSource {
+class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
+    @IBOutlet weak var thePickerView: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        thePickerView.dataSource = self
+        thePickerView.delegate = self
+    }
+
+    //MARK:Picker Delegate & DataSource
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "欄：\(component) 行:\(row)"
+    }
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+        return 4
     }
-    
-
-    @IBOutlet weak var thePickerView: UIPickerView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        thePickerView.dataSource = self
-    }
-
-
 }
-
