6a419b060bcd272b5b598d4cb890c602fd04e9ef
Danny Shen <shenfive@gmail.com>
2022-09-24 16:20:06 +0800

    Data Set up

--- A902HelloPickerView/ViewController.swift
+++ A902HelloPickerView/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource{
     @IBOutlet weak var thePickerView: UIPickerView!
+    
+    var astrological = ["請選擇你的星座","白羊宮","金牛宮","雙子宮","巨蟹宮","獅子宮","處女宮","天秤宮","天蠍宮","射手宮","摩羯宮","水瓶宮","雙魚宮"]
+    var bloudType = ["請選擇你的血型","A","B","O","AB"]
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         thePickerView.dataSource = self
@@ -21,15 +27,33 @@ class ViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSour
         print("row:\(row)  com:\(component)")
     }
     
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "欄：\(component) 行:\(row)"
-    }
     
+    
+    //顯示文字
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch component{
+        case 0:
+            return bloudType[row]
+        case 1:
+            return astrological[row]
+        default:
+            return ""
+        }
+    }
+    //幾欄
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 2
     }
     
+    //幾 row
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 4
+        switch component{
+        case 0:
+            return bloudType.count
+        case 1:
+            return astrological.count
+        default:
+            return 0
+        }
     }
 }
